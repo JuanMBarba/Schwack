359c292b0123f4bf000e7c754238c97058b29cc8
Juan Barba <barbajuan11@gmail.com>
2021-06-01 16:21:43 -0700

    Setup reducers needed for users reducer, Work on users reducer

---
+++ frontend/reducers/root.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities';
+import sessionReducer from './session';
+import errorsReducer from './errors';
+
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
+})
+
+export default rootReducer;
