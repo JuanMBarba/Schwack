9099da79664cf3f2cc18a75eb9976cba2b05cab1
Juan Barba <barbajuan11@gmail.com>
2021-06-01 12:30:33 -0700

    Finish webpack and npm setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { Module } = require('webpack');
+
+Module.exports = {
+    context: __dirname,
+    entry: './frontend/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'inline-source-map'
+}
