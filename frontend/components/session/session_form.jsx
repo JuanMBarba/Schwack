a78dc9088f832b830820468fb86f7a9015ea164d
Juan Barba <barbajuan11@gmail.com>
2021-06-02 14:53:00 -0700

    Complete login and sign up functionality, need to add redirect on success

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,31 +1,58 @@
 import React from "react";
 
     class SessionForm extends React.Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                displayName: "",
+                email:"",
+                password:""
+            }
+            this.handleSubmit = this.handleSubmit.bind(this);
+            this.renderDisplayNameInput = this.renderDisplayNameInput.bind(this);
+        }
 
         renderDisplayNameInput(){
             return (
                 <label>
                     Display Name:
-                    <input type="text" />
+                    <input onChange={this.update("displayName")} type="text" value={this.state.displayName}/>
                 </label>
             )
+        }
+
+        handleSubmit(e){
+            e.preventDefault();
+            this.props.formAction(this.state).then()
+        }
+
+        update(field){
+            return e => this.setState({[field]: e.target.value})
         }
 
         render() {
             return (
                 <div>
                     <h1>{this.props.formType}</h1>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         {this.props.formType === "Sign Up" ? this.renderDisplayNameInput():""}
                         <label>
                             Email:
-                            <input type="text" />
+                            <input onChange={this.update("email")} type="text" value={this.state.email}/>
                         </label>
                         <label>
                             Password:
-                            <input type="password" />
+                            <input onChange={this.update("password")} type="password" value={this.state.password}/>
                         </label>
+                        <button>{this.props.formType}</button>
                     </form>
+                    <div className="form-errors">
+                        <ul>
+                            {this.props.errors.map((error, index) => {
+                                return <li key={index}>error</li>
+                            })}
+                        </ul>
+                    </div>
                 </div>
             )
         }
