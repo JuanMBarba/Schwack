e55803b4c3287649db728c834e7f8e7dfa18b6ab
Juan Barba <barbajuan11@gmail.com>
2021-06-03 15:04:24 -0700

    Set up some ajax requests for messages

---
+++ frontend/util/message_api.js
@@ -0,0 +1,30 @@
+
+export const fetchMessages = () => {
+    return $.ajax({
+        method: "GET",
+        url: "/api/messages"
+    })
+}
+
+export const createMessage = (message) => {
+    return $.ajax({
+        method: "POST",
+        url: "/api/messages",
+        data: { message }
+    })
+}
+
+export const updateMessage = (message) => {
+    return $.ajax({
+        method: "PATCH",
+        url: `/api/messages/${message.id}`,
+        data: { message }
+    })
+}
+
+export const deleteMessage = (messageId) => {
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/messages/${messageId}`
+    })
+}
