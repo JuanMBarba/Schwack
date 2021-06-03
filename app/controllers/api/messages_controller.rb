d32dd9f0fc95321d0de51026dc9c74007ef8df08
Juan Barba <barbajuan11@gmail.com>
2021-06-03 11:58:08 -0700

    Create index action and json response

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,11 @@
 class Api::MessagesController < ApplicationController
     def index
+        @messages = Message.all
+        render "api/messages/index"
     end
 
     def create
+        
     end
 
     def update
@@ -11,4 +14,8 @@ class Api::MessagesController < ApplicationController
     def destroy
     end
 
+    private
+    def message_params
+        params.require(:message).permit(:user_id, :channel_id, :body)
+    end
 end
