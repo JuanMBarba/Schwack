a9b382ec30b2db610ea5e0878258c173e80c0740
Juan Barba <barbajuan11@gmail.com>
2021-06-03 11:25:16 -0700

    Update files to enable logger only during development

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,21 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../reducers/root";
 import thunk from "redux-thunk"
-import logger from "redux-logger";
+// import logger from "redux-logger";
+const middlewares = [thunk];
+
+//Add logger only during non-production
+if (process.env.NODE_ENV !== "production") {
+    // must use 'require' (import only allowed at top of file)
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
     return createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk)) //Logger Removed For Production App
+        applyMiddleware(...middlewares))
 }
 
 export default configureStore;
