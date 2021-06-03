45b076abd3a157242639be1b5b6d7d91dc0ddc06
Juan Barba <barbajuan11@gmail.com>
2021-06-03 10:59:26 -0700

    Add associations/validations to channel model

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,10 @@
 class Channel < ApplicationRecord
+    validates :name, presence: true
+    validates :dm_channel, presence: true, inclusion: { in: [true, false]}
+
+    has_many :messages,
+        primary_key: :id,
+        foreign_key: :channel_id,
+        class_name: :Message
+
 end
