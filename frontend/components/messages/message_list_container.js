ff2d9f5af4d01fdcd1eec5feda175e82f662bd86
Juan Barba <barbajuan11@gmail.com>
2021-06-03 17:33:09 -0700

    Update message list container

--- frontend/components/messages/message_list_container.js
+++ frontend/components/messages/message_list_container.js
@@ -1,14 +1,15 @@
 import { connect } from "react-redux";
-
+import { fetchMessages } from "../../actions/message"
 
 const mSTP = (state) => {
     return {
-        
+        users: state.entities.users,
+        messages: state.entities.messages
     }
 }
 const mDTP = (dispatch) => {
     return {
-
+        fetchMessages: () => dispatch(fetchMessages())
     }
 }
 
