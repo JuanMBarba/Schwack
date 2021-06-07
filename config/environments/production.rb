a8f2a979402881ac3a738709ae5b10d47a11ea2c
Juan Barba <barbajuan11@gmail.com>
2021-06-07 14:40:02 -0700

    Update more files for action cable on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.web_socket_server_url = "wss://schwack.herokuapp.com" 
+  config.action_cable.allowed_request_origins = ['https://schwack.herokuapp.com', 'http://schwack.herokuapp.com']
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
