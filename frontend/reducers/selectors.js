9fccc749d44bb866a7ca21d821cae6ea3d8156a0
Juan Barba <barbajuan11@gmail.com>
2021-06-09 17:19:21 -0700

    Add getUserChannels selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,4 +1,15 @@
-// const getUsersChannels 
+export const getUsersChannels = (currentUser, channels) => {
+    let userChannels = [];
+
+    if (currentUser.channelIds){
+            for (let i = 0; i < currentUser.channelIds.length; i++) {
+            if (channels[currentUser.channelIds[i]]){
+                userChannels.push(channels[currentUser.channelIds[i]])
+            }
+    }}
+
+    return userChannels;
+}
 
 export const getChannelMessages = (messages, channelId) => {
     
