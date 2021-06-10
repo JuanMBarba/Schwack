a5a00dfcd277abe89225df814ccec0fdca70e5bb
Juan Barba <barbajuan11@gmail.com>
2021-06-10 12:11:54 -0700

    Add modal to form

--- frontend/components/channels/channel_form.jsx
+++ frontend/components/channels/channel_form.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 
 class ChannelForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props.channel;
+    }
+
+
+
     render(){
-        return null;
+        return (
+            <div className=".modal">
+                <div class="modal-screen"></div>
+                <form onSubmit={}>
+                    <label>
+                        Name
+                        <input type="text" onChange={} placeholder="e.g. project-plans, questions"/>
+                    </label>
+                    <label>
+                        Description
+                        <input type="text" onChange={} placeholder="(optional)"/>
+                    </label>
+                </form>
+            </div>
+        )
+            
     }
 }
 
