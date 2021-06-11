0c6ef3ed7249fcde3a7dd1c81c8229daa6221295
Juan Barba <barbajuan11@gmail.com>
2021-06-11 10:24:32 -0700

    remove comments

--- frontend/util/channel_subscribe.js
+++ frontend/util/channel_subscribe.js
@@ -1,16 +1,13 @@
 import { removeMessage, receiveMessage } from "../actions/message"
 const createConnection = (currentUserId, channelId, dispatch) => {
-    // console.log(membership)
     return App.cable.subscriptions.create(
         { channel: "ChatChannel", id: channelId },
         {
             received: data => {
-                // console.log("received")
                 if (data.type === "REMOVE_MESSAGE") {
                     dispatch(removeMessage(data.messageId))
                 }
                 else if (data.message.userId !== currentUserId) {
-                    // console.log("hello")
                     dispatch(receiveMessage(data.message));
                 }
             },
