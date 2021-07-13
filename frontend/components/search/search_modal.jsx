0791b3946df8585db8ee8fbe54c00e2a13002b0c
Juan Barba <barbajuan11@gmail.com>
2021-07-13 12:23:34 -0700

    Start search modal

--- frontend/components/search/search_modal.jsx
+++ frontend/components/search/search_modal.jsx
@@ -5,8 +5,8 @@ class SearchModal extends React.Component {
     render(){
         return (
             <div className={`modal${this.props.modalActive ? " active" : ""}`}>
-                <div onClick={() => this.props.switchModalActivity()} className="modal-screen"></div>
-                <div className="search-modal">
+                <div onClick={() => this.props.switchModalActivity()} className="modal-screen transparent"></div>
+                <div className="modal-search">
                     <input type="text" name="search-bar" id="search-bar" />
                     YESSS
                 </div>
